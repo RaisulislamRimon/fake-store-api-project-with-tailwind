14b3d9861d44e85000b2292629f2f84aae5f25f0
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-02 06:20:03 +0000

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+const loadProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      displayProducts(data);
+    });
+};
+
+const displayProducts = (data) => {
+  let products = data
+    .map((product) => {
+      return `<div class="product">
+        <img src="${product.image}" alt="${product.title}">
+        <h2>${product.title}</h2>
+        <p>${product.description}</p>
+        <p>${product.price}</p>
+        <button class="add-to-cart btn btn-primary" data-id="${product.id}">Add to cart</button>
+        </div>`;
+    })
+    .join("");
+  document.getElementById("products").innerHTML = products;
+};
+
+loadProducts();
